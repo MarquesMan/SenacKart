ec038ffcc2c1d028c16e2a4d64cf71891f506f47
MarquesMan <marquesnavarezi@hotmail.com>
2021-03-08 10:07:22 -0400

    Aula_4_Nome_Do_Jogador

--- Assets/Scripts/PlayerKart.cs
+++ Assets/Scripts/PlayerKart.cs
@@ -7,18 +7,39 @@ using Mirror;
 public class PlayerKart : NetworkBehaviour
 {
     // Start is called before the first frame update
+    [SyncVar(hook =nameof(SetTextMeshValue))]
+    private string playerName = "Missing";
+
+    private void SetTextMeshValue(string oldName, string newName)
+    {
+        GetComponentInChildren<TMPro.TextMeshPro>().SetText(newName);
+    }
+
+    [Command]
+    private void setPlayerName(string newName)
+    {
+        playerName = newName;
+    }
+
     void Start()
     {
         if (isLocalPlayer)
         {
             FindObjectOfType<Cinemachine.CinemachineVirtualCamera>().LookAt = transform;
             FindObjectOfType<Cinemachine.CinemachineVirtualCamera>().Follow = this.transform;
+            setPlayerName(PlayerPrefs.GetString("playerName", "Missing"));
         }
         else
         {
-            GetComponent<KartGame.KartSystems.ArcadeKart>().m_Inputs = new KartGame.KartSystems.IInput[] { };
+            try
+            {
+                GetComponent<KartGame.KartSystems.ArcadeKart>().m_Inputs = new KartGame.KartSystems.IInput[] { };
+            }catch (System.NullReferenceException e)
+            {
+                //pass
+            }
 
-            foreach(CapsuleCollider capsuleCollider in GetComponentsInChildren<CapsuleCollider>())
+            foreach (CapsuleCollider capsuleCollider in GetComponentsInChildren<CapsuleCollider>())
             {
                 capsuleCollider.gameObject.layer = LayerMask.NameToLayer("OtherKart");
             }
